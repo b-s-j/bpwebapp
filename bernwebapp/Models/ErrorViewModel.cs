147d16dacb6dd513d11e18871c3da9475bb5bd63
Bern <paul25bern@gmail.com>
2024-02-06 15:03:12 +0800

    Add project files.

---
+++ bernwebapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bernwebapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
