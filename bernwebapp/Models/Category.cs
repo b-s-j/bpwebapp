ad2a5811b22d01212c3be15eea1a69ffd3b089c4
Bern <paul25bern@gmail.com>
2024-02-07 15:37:07 +0800

    REMOVED BOOTSTRAP TOASTR AND USE TOASTR JS

--- bernwebapp/Models/Category.cs
+++ bernwebapp/Models/Category.cs
@@ -8,11 +8,16 @@ namespace bernwebapp.Models
         [Key]
         public int Id { get; set; }
 
-        
+        [Required]
         [DisplayName("Category Name")]
-        public required string? Name { get; set; }
+        [MaxLength(50)]
+        public string? Name { get; set; }
 
         [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be 1 to 100 only.")]
         public int DisplayOrder { get; set; }   
+
+
+
     }
 }
---
+++ bernwebapp/Models/EmployeeEntity.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bernwebapp.Models
+{
+    public class EmployeeEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+        public int EmployeeId { get; set; } = 0;
+
+        public string Office { get; set; }
+
+        [Required]
+        public int Salary { get; set; }
+
+        public string Image { get; set; }
+        public string Email { get; set; }
+    }
+
+
+}
