6444236a3b4b36d545382788b8efd534d1b47ce8
Bern <paul25bern@gmail.com>
2024-03-05 14:58:52 +0800

    #update: Adding Razor project pages different from MVC

---
+++ bpwebapprazorpages/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace bpwebapprazorpages.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(50)]
+        public string? Name { get; set; }
+
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be 1 to 100 only.")]
+        public int DisplayOrder { get; set; }
+
+
+
+    }
+}
